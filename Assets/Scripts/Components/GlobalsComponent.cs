5fc912c30b505bfef341edddc5d88716dbdba6cf
Vlad Matveev <matveev_vlad@me.com>
2020-08-21 14:53:16 +0300

    Migrate project to DOTS

---
+++ Assets/Scripts/Components/GlobalsComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public struct GlobalsComponent : ISharedComponentData, IEquatable<GlobalsComponent>
+{
+    public ScriptableObject _data;
+    
+    public bool Equals(GlobalsComponent other)
+    {
+        return _data == other._data;
+    }
+
+    public override int GetHashCode()
+    {
+        return _data.GetHashCode();
+    }
+}
